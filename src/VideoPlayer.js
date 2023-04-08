c87b5da3523cf439aabc49b40ba1efc415173b2a
diogobalieiro <diogobalieirovedovello@gmail.com>
2023-04-07 23:51:55 -0300

    eighth commit

--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import videoFile from './video/sinaldivino.mp4';
-
+import ReactPlayer from 'react-player';
 
 const VideoPlayer = () => {
   return (
-    <div className="video-player" style={{width:'100%'}}>
-      <video controls width="100%" height="100%">
-        <source src={videoFile} type="video/mp4" />
-        Seu navegador não suporta o elemento de vídeo.
-      </video>
+    <div className="video-player" style={{width:'100%', height:'100%'}}>
+      {/* <iframe 
+        src="https://player.vimeo.com/video/815784967?h=52b5dbcd85&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" 
+        title="sinaldivino.mp4"
+        width="100%"
+        height="100%"
+        
+      ></iframe> */}
+      <ReactPlayer
+        url="https://vimeo.com/815784967"
+        width="100%"
+        height="100%"
+        controls
+      />
     </div>
   );  
 };
